8b4e680db659394f4c5b0ff99cf357587bc79906
David Elvira Martin <delvira-@c2r9s2.42malaga.com>
2023-03-02 19:56:26 +0100

    Creada la función que_search
    Esta función busca un elemento en la cola

--- icarboni-delvira-cejimene/datalib/que_stats.c
+++ icarboni-delvira-cejimene/datalib/que_stats.c
@@ -6,7 +6,7 @@
 /*   By: delvira- <delvira-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/03/02 19:54:58 by delvira-         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:56:12 by delvira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,11 @@ t_queue	*que_last(t_queue *queue)
 
 t_queue	*que_search(t_queue *queue, void *data)
 {
-	// TODO
+		while (queue)
+	{
+		if (queue->data == data)
+			return (queue);
+		queue = queue->next;
+	}
+	return (NULL);
 }
