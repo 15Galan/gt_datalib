877b04db1fa758a3e7ff50bcf2d575ea4d5fcca8
Luis Francisco Morales Maldonado <luiste49@gmail.com>
2023-03-02 19:24:12 +0100

    Funciones new, add_first y add_after
    Funciones realizadas

--- lumorale-fsantama-gfredes-/datalib/dll_creators.c
+++ lumorale-fsantama-gfredes-/datalib/dll_creators.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dll_creators.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: lumorale <lumorale@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 11:55:26 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:23:20 by lumorale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,38 @@
 
 t_dlist	*dll_new(void *data)
 {
-	// TODO
+	t_dlist	*new;
+
+	new = (t_dlist *) malloc(sizeof(t_dlist));
+	if (!new)
+		return (NULL);
+	new->data = data;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
 }
 
 void	dll_add_first(t_dlist **list, t_dlist *elem)
 {
-	// TODO
+	if (dll_empty(*list))
+		*list = elem;
+	else
+	{
+		elem->next = *list;
+		(*list)->prev = elem;
+		*list = elem;
+	}
 }
 
 void	dll_add_after(t_dlist *elem, t_dlist *new)
 {
-	// TODO
+	if (dll_empty(elem) || dll_empty(new))
+		return ;
+	new->next = elem->next;
+	new->prev = elem;
+	if (elem->next)
+		elem->next->prev = new;
+	elem->next = new;
 }
 
 void	dll_add_last(t_dlist **list, t_dlist *elem)
