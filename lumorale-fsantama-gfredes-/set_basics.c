b15897adde34a2161beba05daaed6d49d4f609c4
fsantama <fransantamaria95@gmail.com>
2023-03-02 19:30:56 +0100

    Función set_basics.c creada

---
+++ lumorale-fsantama-gfredes-/set_basics.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_basics.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 20:44:57 by antgalan          #+#    #+#             */
+/*   Updated: 2023/03/02 19:29:51 by fsantama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "datalib.h"
+
+t_set	*set_new(void *data)
+{
+	// TODO
+}
+
+void	set_add(t_set **set, void *data)
+{
+	// TODO
+}
+
+void	set_remove(t_set **set, void *data)
+{
+	// TODO
+}
+
+void	set_clear(t_set **set)
+{
+	// TODO
+}
