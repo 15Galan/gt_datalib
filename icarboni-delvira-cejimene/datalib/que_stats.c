96f3c0903256537e4f6aed0dbb9447c26946265c
David Elvira Martin <delvira-@c2r9s2.42malaga.com>
2023-03-02 19:51:42 +0100

    Creada la función que_empty
    Esta función checkea si la cola esta vacia

--- icarboni-delvira-cejimene/datalib/que_stats.c
+++ icarboni-delvira-cejimene/datalib/que_stats.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   que_stats.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: delvira- <delvira-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 12:04:00 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:51:30 by delvira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	que_empty(t_queue *queue)
 {
-	// TODO
+	return (queue == NULL);
 }
 
 int	que_size(t_queue *queue)
