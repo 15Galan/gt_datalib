fdb0dab7782b988f1969af885f39291d21d4a01a
Mickael Bruzzi <mbruzzi@c3r2s3.42malaga.com>
2023-06-27 14:12:59 +0200

    que_stats.c
    function que_size

--- 00-projects-template/datalib/que_stats.c
+++ 00-projects-template/datalib/que_stats.c
@@ -6,7 +6,7 @@
 /*   By: mbruzzi <mbruzzi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/06/27 14:10:07 by mbruzzi          ###   ########.fr       */
+/*   Updated: 2023/06/27 14:12:52 by mbruzzi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,15 @@ int	que_empty(t_queue *queue)
 
 int	que_size(t_queue *queue)
 {
-	// TODO
+	int	i;
+
+	i = 0;
+	while (queue)
+	{
+		queue = queue->next;
+		i++;
+	}
+	return (i);
 }
 
 t_queue	*que_first(t_queue *queue)
