36a9f3127f3e5e3e1941737002f2c774cdf729f5
Librada Justicia ruiz <ljustici@c3r2s2.42malaga.com>
2023-06-27 14:23:06 +0200

    Función que retorna el tamaño de lista
    Retorna el tamaño de lista

--- svillav-mbruzzi-ljustici/datalib/dll_stats.c
+++ svillav-mbruzzi-ljustici/datalib/dll_stats.c
@@ -6,7 +6,7 @@
 /*   By: ljustici <ljustici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/06/27 14:20:58 by ljustici         ###   ########.fr       */
+/*   Updated: 2023/06/27 14:23:02 by ljustici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,15 @@ int	dll_empty(t_dlist *list)
 
 int	dll_size(t_dlist *list)
 {
-	// TODO
+	int	i;
+
+	i = 0;
+	while (list)
+	{
+		list = list->next;
+		i++;
+	}
+	return (i);
 }
 
 t_dlist	*dll_first(t_dlist *list)
