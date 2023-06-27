7d8474c1aa95042fc833380548c6b4f3f37c6f50
Librada Justicia ruiz <ljustici@c3r2s2.42malaga.com>
2023-06-27 14:26:01 +0200

    Función que devuelve el último elemento
    Devuelve el último elemento de la lista

--- svillav-mbruzzi-ljustici/datalib/dll_stats.c
+++ svillav-mbruzzi-ljustici/datalib/dll_stats.c
@@ -6,7 +6,7 @@
 /*   By: ljustici <ljustici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/06/27 14:23:34 by ljustici         ###   ########.fr       */
+/*   Updated: 2023/06/27 14:24:06 by ljustici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,9 @@ t_dlist	*dll_first(t_dlist *list)
 
 t_dlist	*dll_last(t_dlist *list)
 {
-	// TODO
+	while (list && list->next)
+		list = list->next;
+	return (list);
 }
 
 t_dlist	*dll_search(t_dlist *list, void *data)
