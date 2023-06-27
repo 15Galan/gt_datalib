dfe040aa8d1014bac1fd4abe54b4fb1af3ce5683
Sabrina Villalva <svillalv@c3r6s1.42malaga.com>
2023-06-27 16:49:43 +0200

    stk_push function
    Adds a node at beggining of the stack

--- svillav-mbruzzi-ljustici/datalib/stk_basics.c
+++ svillav-mbruzzi-ljustici/datalib/stk_basics.c
@@ -6,7 +6,7 @@
 /*   By: svillalv <svillalv@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2023/06/27 16:48:48 by svillalv         ###   ########.fr       */
+/*   Updated: 2023/06/27 16:49:33 by svillalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,13 @@ t_stack	*stk_new(void *data)
 
 void	stk_push(t_stack **stack, void *data)
 {
-	// TODO
+	t_stack	*new;
+
+	new = stk_new(data);
+	if (new == NULL)
+		return ;
+	new->next = *stack;
+	*stack = new;
 }
 
 t_stack	*stk_pop(t_stack **stack)
