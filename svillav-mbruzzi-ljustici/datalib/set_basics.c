9e0a61b9eabbea8e6c2b2cc1688ffa5f5c41a6af
Sabrina Villalva <svillalv@c3r2s1.42malaga.com>
2023-06-27 14:19:51 +0200

    set_remove function
    Borra conjunto

--- svillav-mbruzzi-ljustici/datalib/set_basics.c
+++ svillav-mbruzzi-ljustici/datalib/set_basics.c
@@ -6,7 +6,7 @@
 /*   By: svillalv <svillalv@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 20:44:57 by antgalan          #+#    #+#             */
-/*   Updated: 2023/06/27 14:14:31 by svillalv         ###   ########.fr       */
+/*   Updated: 2023/06/27 14:19:30 by svillalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,27 @@ void	set_add(t_set **set, void *data)
 
 void	set_remove(t_set **set, void *data)
 {
-	// TODO
+	t_set	*aux;
+	t_set	*prev;
+
+	if (!set_contains(*set, data))
+		return ;
+	aux = *set;
+	prev = NULL;
+	while (aux)
+	{
+		if (aux->data == data)
+		{
+			if (prev)
+				prev->next = aux->next;
+			else
+				*set = aux->next;
+			free(aux);
+			return ;
+		}
+		prev = aux;
+		aux = aux->next;
+	}
 }
 
 void	set_clear(t_set **set)
