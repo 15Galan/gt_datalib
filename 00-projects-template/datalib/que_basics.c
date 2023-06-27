46429b33427760dbcc0a019bcda8a92bd14d533c
Mickael Bruzzi <mbruzzi@c3r2s3.42malaga.com>
2023-06-27 13:49:49 +0200

    que_basics.c
    function que_new made

--- 00-projects-template/datalib/que_basics.c
+++ 00-projects-template/datalib/que_basics.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   que_basics.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: mbruzzi <mbruzzi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 12:03:22 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/06/27 13:49:03 by mbruzzi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 t_queue	*que_new(void *data)
 {
-	// TODO
+	t_queue	*new;
+
+	new = (t_queue *) malloc(sizeof(t_queue));
+	if (!new)
+		return (NULL);
+	new->data = data;
+	new->next = NULL;
+	return (new);
 }
 
 void	que_enqueue(t_queue **queue, void *data)
