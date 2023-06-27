60048b9f67375ef08f383054302469ea0af0bdef
Librada Justicia ruiz <ljustici@c3r2s2.42malaga.com>
2023-06-27 13:17:48 +0200

    first commit

---
+++ svillav-mbruzzi-ljustici/datalib/set_stats.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_stats.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 21:20:09 by antgalan          #+#    #+#             */
+/*   Updated: 2023/01/23 20:57:22 by antgalan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "datalib.h"
+
+int	set_empty(t_set *set)
+{
+	// TODO
+}
+
+int	set_contains(t_set *set, void *data)
+{
+	// TODO
+}
+
+int	set_subset(t_set *set1, t_set *set2)
+{
+	// TODO
+}
+
+int	set_equal(t_set *set1, t_set *set2)
+{
+	// TODO
+}
+
+int	set_size(t_set *set)
+{
+	// TODO
+}
