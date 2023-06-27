d3b330755b4e26a86a29be1ede3c60b949a29a5f
Sabrina Villalva <svillalv@c3r6s1.42malaga.com>
2023-06-27 16:51:24 +0200

    stk_pop function
    Aisla un nodo, next apunta a null

--- svillav-mbruzzi-ljustici/datalib/stk_basics.c
+++ svillav-mbruzzi-ljustici/datalib/stk_basics.c
@@ -6,7 +6,7 @@
 /*   By: svillalv <svillalv@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2023/06/27 16:49:33 by svillalv         ###   ########.fr       */
+/*   Updated: 2023/06/27 16:51:16 by svillalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,14 @@ void	stk_push(t_stack **stack, void *data)
 
 t_stack	*stk_pop(t_stack **stack)
 {
-	// TODO
+	t_stack	*aux;
+
+	if (*stack == NULL)
+		return (NULL);
+	aux = *stack;
+	*stack = (*stack)->next;
+	aux->next = NULL;
+	return (aux);
 }
 
 void	stk_clear(t_stack **stack)
