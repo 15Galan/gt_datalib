bb0dfbf007aff379e169631f67d796e5e89317ba
Sabrina Villalva <svillalv@c3r6s1.42malaga.com>
2023-06-27 16:21:28 +0200

    set_diff
    Establece datos en los nodos que no los contengan

--- svillav-mbruzzi-ljustici/datalib/set_maths.c
+++ svillav-mbruzzi-ljustici/datalib/set_maths.c
@@ -6,7 +6,7 @@
 /*   By: svillalv <svillalv@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:48:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/06/27 16:19:01 by svillalv         ###   ########.fr       */
+/*   Updated: 2023/06/27 16:21:10 by svillalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,16 @@ t_set	*set_inter(t_set *set1, t_set *set2)
 
 t_set	*set_diff(t_set *set1, t_set *set2)
 {
-	// TODO
+	t_set	*aux;
+	t_set	*new;
+
+	new = NULL;
+	aux = set1;
+	while (aux)
+	{
+		if (!set_contains(set2, aux->data))
+			set_add(&new, aux->data);
+		aux = aux->next;
+	}
+	return (new);
 }
