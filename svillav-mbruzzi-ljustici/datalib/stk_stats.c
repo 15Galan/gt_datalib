28ed962e57ac8ede1f4528ecd880922319e7cf76
Librada Justicia ruiz <ljustici@c3r6s2.42malaga.com>
2023-06-27 16:44:20 +0200

    Función que devuelve el primer elemento
    Devuelve el primer elemento del stack

--- svillav-mbruzzi-ljustici/datalib/stk_stats.c
+++ svillav-mbruzzi-ljustici/datalib/stk_stats.c
@@ -6,7 +6,7 @@
 /*   By: ljustici <ljustici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2023/06/27 16:43:31 by ljustici         ###   ########.fr       */
+/*   Updated: 2023/06/27 16:44:12 by ljustici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,9 @@ int	stk_size(t_stack *stack)
 
 t_stack	*stk_top(t_stack *stack)
 {
-	// TODO
+	if (stk_empty(stack))
+		return (NULL);
+	return (stack);
 }
 
 void	*stk_peek(t_stack *stack)
