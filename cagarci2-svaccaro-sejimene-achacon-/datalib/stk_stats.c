c6fe7d38115d95344d5036225e63542b9755833b
Susana Paoli Vaccaro Nunez <svaccaro@c1r11s4.42malaga.com>
2023-10-18 21:10:14 +0200

    Completar stk_stats.c
    Se ha completado el código de las funciones stk_empty, stk_size,
    stk_top, stk_peek que se encuentran dentro del archivo stk_stats.c

--- cagarci2-svaccaro-sejimene-achacon-/datalib/stk_stats.c
+++ cagarci2-svaccaro-sejimene-achacon-/datalib/stk_stats.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 12:09:21 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/10/18 20:59:58 by svaccaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,32 @@
 
 int	stk_empty(t_stack *stack)
 {
-	// TODO
+	return (stack == NULL);
 }
 
 int	stk_size(t_stack *stack)
 {
-	// TODO
+	int	size;
+
+	size = 0;
+	while (stack)
+	{
+		size++;
+		stack = stack->next;
+	}
+	return (size);
 }
 
 t_stack	*stk_top(t_stack *stack)
 {
-	// TODO
+	if (stk_empty(stack))
+		return (NULL);
+	return (stack);
 }
 
 void	*stk_peek(t_stack *stack)
 {
-	// TODO
+	if (stk_empty(stack))
+		return (NULL);
+	return (stack->data);
 }
