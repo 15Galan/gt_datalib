9896c57dc889d33c547695d5c5d0e072abdbd3e3
Jorge Mihalyczo <jmihalyc@c1r10s1.42malaga.com>
2023-10-18 21:13:17 +0200

    Complete stk_basics.c
    Functions completed: stk_new, stk_push, stk_pop, stk_clear

--- lpin.1-jmihalyc.2-febasma.3/datalib/stk_basics.c
+++ lpin.1-jmihalyc.2-febasma.3/datalib/stk_basics.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 12:08:46 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/10/18 21:10:55 by jmihalyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,47 @@
 
 t_stack	*stk_new(void *data)
 {
-	// TODO
+	t_stack	*new;
+
+	new = (t_stack *) malloc(sizeof(t_stack));
+	if (new == NULL)
+		return (NULL);
+	new->data = data;
+	new->next = NULL;
+	return (new);
 }
 
 void	stk_push(t_stack **stack, void *data)
 {
-	// TODO
+	t_stack	*new;
+
+	new = stk_new(data);
+	if (new == NULL)
+		return ;
+	new->next = *stack;
+	*stack = new;
 }
 
 t_stack	*stk_pop(t_stack **stack)
 {
-	// TODO
+	t_stack	*aux;
+
+	if (*stack == NULL)
+		return (NULL);
+	aux = *stack;
+	*stack = (*stack)->next;
+	aux->next = NULL;
+	return (aux);
 }
 
 void	stk_clear(t_stack **stack)
 {
-	// TODO
+	t_stack	*aux;
+
+	while (*stack != NULL)
+	{
+		aux = *stack;
+		*stack = (*stack)->next;
+		free(aux);
+	}
 }
