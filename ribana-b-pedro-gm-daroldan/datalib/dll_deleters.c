d0da6bf520265288533cc6bccb530be489ce2a6b
SrRecursive <rojohnibana@gmail.com>
2023-10-18 22:18:13 +0200

    Fix dll_deleters.c

--- ribana-b-pedro-gm-daroldan/datalib/dll_deleters.c
+++ ribana-b-pedro-gm-daroldan/datalib/dll_deleters.c
@@ -14,25 +14,71 @@
 
 void	dll_remove_first(t_dlist **list)
 {
-	// TODO
+	t_dlist	*aux;
+
+	if (dll_empty(*list))
+		return ;
+	aux = *list;
+	*list = (*list)->next;
+	if (*list)
+		(*list)->prev = NULL;
+	free(aux);
 }
 
 void	dll_remove(t_dlist **list, t_dlist *elem)
 {
-	// TODO
+	if (dll_empty(*list))
+		return ;
+	if (elem->prev)
+		elem->prev->next = elem->next;
+	else
+		*list = elem->next;
+	if (elem->next)
+		elem->next->prev = elem->prev;
+	free(elem);
 }
 
 void	dll_remove_last(t_dlist **list)
 {
-	// TODO
+	t_dlist	*aux;
+
+	if (dll_empty(*list))
+		return ;
+	aux = *list;
+	while (aux->next)
+		aux = aux->next;
+	if (aux->prev)
+		aux->prev->next = NULL;
+	else
+		*list = NULL;
+	free(aux);
 }
 
 void	dll_purge(t_dlist **list, void *data)
 {
-	// TODO
+	t_dlist	*aux;
+
+	if (dll_empty(*list))
+		return ;
+	aux = *list;
+	while (aux)
+	{
+		if (aux->data == data)
+			dll_remove(list, aux);
+		aux = aux->next;
+	}
 }
 
 void	dll_clear(t_dlist **list)
 {
-	// TODO
+	t_dlist	*aux;
+
+	if (dll_empty(*list))
+		return ;
+	while (*list)
+	{
+		aux = *list;
+		*list = (*list)->next;
+		free(aux);
+	}
 }
