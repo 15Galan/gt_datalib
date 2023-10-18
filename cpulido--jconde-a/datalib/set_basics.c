ddc8aa732539f9c95fedd187bb0234e55bfc0aa7
Christian Pulido Navarrete <cpulido-@c1r9s3.42malaga.com>
2023-10-18 20:51:25 +0200

    Prueba push

---
+++ cpulido--jconde-a/datalib/set_basics.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_basics.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 20:44:57 by antgalan          #+#    #+#             */
+/*   Updated: 2023/01/23 20:56:19 by antgalan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "datalib.h"
+
+t_set	*set_new(void *data)
+{
+	// TODO
+}
+
+void	set_add(t_set **set, void *data)
+{
+	// TODO
+}
+
+void	set_remove(t_set **set, void *data)
+{
+	// TODO
+}
+
+void	set_clear(t_set **set)
+{
+	// TODO
+}
