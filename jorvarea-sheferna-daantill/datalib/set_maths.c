4459b6fea61226781cb40b7584a2c37b35ef29f7
Jorge Varea Duran <jorvarea@c1r11s1.42malaga.com>
2023-10-18 21:04:04 +0200

    feat: add utility functions for set properties and checks

--- jorvarea-sheferna-daantill/datalib/set_maths.c
+++ jorvarea-sheferna-daantill/datalib/set_maths.c
@@ -3,26 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   set_maths.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:48:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 20:56:43 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/10/18 21:00:41 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "datalib.h"
+#include <stdlib.h>
+
+int set_contains(t_set *set, void *data)
+{
+	while (set)
+	{
+		if (set->data == data)
+			return (1);
+		set = set->next;
+	}
+	return (0);
+}
 
 t_set	*set_union(t_set *set1, t_set *set2)
 {
-	// TODO
+	t_set *result = NULL;
+	t_set *current;
+
+	// Añadir todos los elementos de set1 al resultado.
+	current = set1;
+	while (current)
+	{
+		set_add(&result, current->data);
+		current = current->next;
+	}
+
+	// Añadir elementos de set2 que no están ya en el resultado.
+	current = set2;
+	while (current)
+	{
+		if (!set_contains(result, current->data))
+		{
+			set_add(&result, current->data);
+		}
+		current = current->next;
+	}
+
+	return result;
 }
 
 t_set	*set_inter(t_set *set1, t_set *set2)
 {
-	// TODO
+	t_set *result = NULL;
+	t_set *current;
+
+	current = set1;
+	while (current)
+	{
+		if (set_contains(set2, current->data))
+		{
+			set_add(&result, current->data);
+		}
+		current = current->next;
+	}
+
+	return result;
 }
 
 t_set	*set_diff(t_set *set1, t_set *set2)
 {
-	// TODO
+	t_set *result = NULL;
+	t_set *current;
+
+	current = set1;
+	while (current)
+	{
+		if (!set_contains(set2, current->data))
+		{
+			set_add(&result, current->data);
+		}
+		current = current->next;
+	}
+
+	return result;
 }
