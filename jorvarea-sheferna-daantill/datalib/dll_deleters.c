08d0fbc75b20a1842317bcca949156354722c8c5
Sheila Fernandez Cisneros <sheferna@c1r11s3.42malaga.com>
2023-10-18 20:35:11 +0200

    adding

---
+++ jorvarea-sheferna-daantill/datalib/dll_deleters.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dll_deleters.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
+/*   Updated: 2023/01/23 11:56:17 by antgalan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "datalib.h"
+
+void	dll_remove_first(t_dlist **list)
+{
+	// TODO
+}
+
+void	dll_remove(t_dlist **list, t_dlist *elem)
+{
+	// TODO
+}
+
+void	dll_remove_last(t_dlist **list)
+{
+	// TODO
+}
+
+void	dll_purge(t_dlist **list, void *data)
+{
+	// TODO
+}
+
+void	dll_clear(t_dlist **list)
+{
+	// TODO
+}
