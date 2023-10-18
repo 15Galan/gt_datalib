ced1440acde43603f53c261a010d7efa53f648c2
Nicob11 <nicobasma_@hotmail.com>
2023-10-18 21:44:33 +0200

    completed que_basics.c file

--- lpin.1-jmihalyc.2-febasma.3/datalib/que_basics.c
+++ lpin.1-jmihalyc.2-febasma.3/datalib/que_basics.c
@@ -6,55 +6,55 @@
 /*   By: febasma <nicobasma_@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/10/18 21:30:09 by febasma          ###   ########.fr       */
+/*   Updated: 2023/10/18 21:38:26 by febasma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "datalib.h"
 
-// t_queue	*que_new(void *data)
-// {
-// 	t_queue	*new;
+t_queue	*que_new(void *data)
+{
+	t_queue	*new;
 
-// 	new = (t_queue *) malloc(sizeof(t_queue));
-// 	if (!new)
-// 		return (NULL);
-// 	new->data = data;
-// 	new->next = NULL;
-// 	return (new);
-// }
+	new = (t_queue *) malloc(sizeof(t_queue));
+	if (!new)
+		return (NULL);
+	new->data = data;
+	new->next = NULL;
+	return (new);
+}
 
-// void	que_enqueue(t_queue **queue, void *data)
-// {
-// 	t_queue	*new;
+void	que_enqueue(t_queue **queue, void *data)
+{
+	t_queue	*new;
 
-// 	new = que_new(data);
-// 	if (!new)
-// 		return ;
-// 	if (que_empty(*queue))
-// 	{
-// 		*queue = new;
-// 		return ;
-// 	}
-// 	que_last(*queue)->next = new;
-// }
+	new = que_new(data);
+	if (!new)
+		return ;
+	if (que_empty(*queue))
+	{
+		*queue = new;
+		return ;
+	}
+	que_last(*queue)->next = new;
+}
 
-// void	*que_dequeue(t_queue **queue)
-// {
-// 	t_queue	*aux;
-// 	void	*data;
+void	*que_dequeue(t_queue **queue)
+{
+	t_queue	*aux;
+	void	*data;
 
-// 	if (que_empty(*queue))
-// 		return (NULL);
-// 	aux = *queue;
-// 	*queue = (*queue)->next;
-// 	data = aux->data;
-// 	free(aux);
-// 	return (data);
-// }
+	if (que_empty(*queue))
+		return (NULL);
+	aux = *queue;
+	*queue = (*queue)->next;
+	data = aux->data;
+	free(aux);
+	return (data);
+}
 
-// void	que_clear(t_queue **queue)
-// {
-// 	while (!que_empty(*queue))
-// 		que_dequeue(queue);
-// }
+void	que_clear(t_queue **queue)
+{
+	while (!que_empty(*queue))
+		que_dequeue(queue);
+}
