62df00bdf02720590649bd3ec28c530c960b54d4
Sergio Aroca Fernandez <saroca-f@c2r3s1.42malaga.com>
2024-05-07 21:16:09 +0200

    set_inter complete
    He copiado correctamente el archivo.

--- saroca-f-schamizo-alvaquer/datalib/set_maths.c
+++ saroca-f-schamizo-alvaquer/datalib/set_maths.c
@@ -6,7 +6,7 @@
 /*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:48:46 by antgalan          #+#    #+#             */
-/*   Updated: 2024/05/07 21:14:48 by saroca-f         ###   ########.fr       */
+/*   Updated: 2024/05/07 21:15:58 by saroca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,18 @@ t_set	*set_union(t_set *set1, t_set *set2)
 
 t_set	*set_inter(t_set *set1, t_set *set2)
 {
-	// TODO
+	t_set	*aux;
+	t_set	*new;
+
+	new = NULL;
+	aux = set1;
+	while (aux)
+	{
+		if (set_contains(set2, aux->data))
+			set_add(&new, aux->data);
+		aux = aux->next;
+	}
+	return (new);
 }
 
 t_set	*set_diff(t_set *set1, t_set *set2)
