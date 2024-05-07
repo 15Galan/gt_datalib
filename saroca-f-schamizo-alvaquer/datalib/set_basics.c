6b8e2a0f8e8f3b09566c95022db40439aa9a9302
Salvador Chamizo <salvadorchamizo@gmail.com>
2024-05-07 20:56:36 +0200

    set_basics, set_clear
    Rellenada la función set_clear en el archivo set_basics

--- saroca-f-schamizo-alvaquer/datalib/set_basics.c
+++ saroca-f-schamizo-alvaquer/datalib/set_basics.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 20:44:57 by antgalan          #+#    #+#             */
-/*   Updated: 2024/05/07 20:25:45 by schamizo         ###   ########.fr       */
+/*   Updated: 2024/05/07 20:56:15 by schamizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,5 +63,14 @@ void	set_remove(t_set **set, void *data)
 
 void	set_clear(t_set **set)
 {
-	// TODO
+	t_set	*aux;
+
+	if (set_empty(*set))
+		return ;
+	while (*set)
+	{
+		aux = *set;
+		*set = (*set)->next;
+		free(aux);
+	}
 }
