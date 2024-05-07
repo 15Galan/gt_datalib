e7305cb367397b67ee28ec2e934dcc105a97d603
Salvador Chamizo <salvadorchamizo@gmail.com>
2024-05-07 21:14:58 +0200

    set_stats, set_subset
    Rellenada la funcion set_subset en el archivo set_stats

--- saroca-f-schamizo-alvaquer/datalib/set_stats.c
+++ saroca-f-schamizo-alvaquer/datalib/set_stats.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:20:09 by antgalan          #+#    #+#             */
-/*   Updated: 2024/05/07 21:10:44 by schamizo         ###   ########.fr       */
+/*   Updated: 2024/05/07 21:14:52 by schamizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,13 @@ int	set_contains(t_set *set, void *data)
 
 int	set_subset(t_set *set1, t_set *set2)
 {
-	// TODO
+	while (set1)
+	{
+		if (!set_contains(set2, set1->data))
+			return (0);
+		set1 = set1->next;
+	}
+	return (1);
 }
 
 int	set_equal(t_set *set1, t_set *set2)
