0d93910162abf47abc5378f599f50f8501f01280
Sergio Aroca Fernandez <saroca-f@c2r3s1.42malaga.com>
2024-05-07 21:15:15 +0200

    set_union completada
    He copiado correctamente la funcion.

--- saroca-f-schamizo-alvaquer/datalib/set_maths.c
+++ saroca-f-schamizo-alvaquer/datalib/set_maths.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   set_maths.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: saroca-f <saroca-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:48:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 20:56:43 by antgalan         ###   ########.fr       */
+/*   Updated: 2024/05/07 21:14:48 by saroca-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 t_set	*set_union(t_set *set1, t_set *set2)
 {
-	// TODO
+	t_set	*aux;
+	t_set	*new;
+
+	new = NULL;
+	aux = set1;
+	while (aux)
+	{
+		set_add(&new, aux->data);
+		aux = aux->next;
+	}
+	aux = set2;
+	while (aux)
+	{
+		set_add(&new, aux->data);
+		aux = aux->next;
+	}
+	return (new);
 }
 
 t_set	*set_inter(t_set *set1, t_set *set2)
