4cfea78d77f20ae050b608fb30cbe52812cb142d
Salvador Chamizo <salvadorchamizo@gmail.com>
2024-05-07 21:09:31 +0200

    set_stats, set_empty
    Rellenada la función set_empty en el archivo set_stats.c

--- saroca-f-schamizo-alvaquer/datalib/set_stats.c
+++ saroca-f-schamizo-alvaquer/datalib/set_stats.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:20:09 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 20:57:22 by antgalan         ###   ########.fr       */
+/*   Updated: 2024/05/07 21:09:15 by schamizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	set_empty(t_set *set)
 {
-	// TODO
+	return (set == NULL);
 }
 
 int	set_contains(t_set *set, void *data)
