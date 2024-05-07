9d4a881ae933d92fa7b2ca787c9d9007f90988f4
Salvador Chamizo <salvadorchamizo@gmail.com>
2024-05-07 20:20:44 +0200

    set_basics.c, set_new()
    Rellenada la función set_new

--- saroca-f-schamizo-alvaquer/datalib/set_basics.c
+++ saroca-f-schamizo-alvaquer/datalib/set_basics.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 20:44:57 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 20:56:19 by antgalan         ###   ########.fr       */
+/*   Updated: 2024/05/07 20:20:25 by schamizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 t_set	*set_new(void *data)
 {
-	// TODO
+	t_set	*new;
+
+	new = (t_set *) malloc(sizeof(t_set));
+	if (!new)
+		return (NULL);
+	new->data = data;
+	new->next = NULL;
+	return (new);
 }
 
 void	set_add(t_set **set, void *data)
