0789d7c1cff71d3e7bd35a0a0bbc3d34ae3b9596
alvaquer <albertovaquero84@gmail.com>
2024-05-07 21:33:46 +0200

    added dll_new
    new function

--- 00-projects-template/datalib/dll_creators.c
+++ 00-projects-template/datalib/dll_creators.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/03/07 20:45:47 by antgalan         ###   ########.fr       */
+/*   Updated: 2024/05/07 21:33:33 by alvaquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 t_dlist	*dll_new(void *data)
 {
-	// TODO
+	t_dlist	*new;
+
+	new = (t_dlist *) malloc(sizeof(t_dlist));
+	if (!new)
+		return (NULL);
+	new->data = data;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
 }
 
 void	dll_add_first(t_dlist **list, t_dlist *elem)
