cf6af5ae2b1703d69d16f3912138596b61f3ad51
albmarqu <albmarqu@student.42malaga.com>
2024-10-08 17:44:19 +0200

    Function: dll remove last

--- albmarqu-glopez-c/datalib/dll_deleters.c
+++ albmarqu-glopez-c/datalib/dll_deleters.c
@@ -6,7 +6,7 @@
 /*   By: albmarqu <albmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2024/10/08 17:43:39 by albmarqu         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:44:04 by albmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,18 @@ void	dll_remove(t_dlist **list, t_dlist *elem)
 
 void	dll_remove_last(t_dlist **list)
 {
-	// TODO
+	t_dlist	*aux;
+
+	if (dll_empty(*list))
+		return ;
+	aux = *list;
+	while (aux->next)
+		aux = aux->next;
+	if (aux->prev)
+		aux->prev->next = NULL;
+	else
+		*list = NULL;
+	free(aux);
 }
 
 void	dll_purge(t_dlist **list, void *data)
