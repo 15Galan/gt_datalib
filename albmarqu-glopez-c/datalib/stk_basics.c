698f4d0a60a2672dbcc32abc3002a7fde4599879
glopez-c <glopez-c@student.42malaga.com>
2024-10-08 17:09:44 +0200

    Added stk_pop() to stk_basics.c

--- albmarqu-glopez-c/datalib/stk_basics.c
+++ albmarqu-glopez-c/datalib/stk_basics.c
@@ -6,7 +6,7 @@
 /*   By: glopez-c <glopez-c@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2024/10/08 17:08:32 by glopez-c         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:09:32 by glopez-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,14 @@ void	stk_push(t_stack **stack, void *data)
 
 t_stack	*stk_pop(t_stack **stack)
 {
-	// TODO
+	t_stack	*aux;
+
+	if (*stack == NULL)
+		return (NULL);
+	aux = *stack;
+	*stack = (*stack)->next;
+	aux->next = NULL;
+	return (aux);
 }
 
 void	stk_clear(t_stack **stack)
