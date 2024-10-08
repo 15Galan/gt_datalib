b10132f5eb9aedd7fe70bc1f5bcccf67de3b940f
glopez-c <glopez-c@student.42malaga.com>
2024-10-08 17:23:10 +0200

    Added stk_size() to stk_stats.c

--- albmarqu-glopez-c/datalib/stk_stats.c
+++ albmarqu-glopez-c/datalib/stk_stats.c
@@ -6,7 +6,7 @@
 /*   By: glopez-c <glopez-c@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2024/10/08 17:21:52 by glopez-c         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:22:57 by glopez-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,15 @@ int	stk_empty(t_stack *stack)
 
 int	stk_size(t_stack *stack)
 {
-	// TODO
+	int	size;
+
+	size = 0;
+	while (stack)
+	{
+		size++;
+		stack = stack->next;
+	}
+	return (size);
 }
 
 t_stack	*stk_top(t_stack *stack)
