9c1999a00f907ab5a8d3563b87503241c71defc3
albmarqu <albmarqu@student.42malaga.com>
2024-10-08 17:43:30 +0200

    Function: dll remove first

--- albmarqu-glopez-c/datalib/dll_deleters.c
+++ albmarqu-glopez-c/datalib/dll_deleters.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dll_deleters.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: albmarqu <albmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 11:56:17 by antgalan         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:43:14 by albmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 void	dll_remove_first(t_dlist **list)
 {
-	// TODO
+	t_dlist	*aux;
+
+	if (dll_empty(*list))
+		return ;
+	aux = *list;
+	*list = (*list)->next;
+	if (*list)
+		(*list)->prev = NULL;
+	free(aux);
 }
 
 void	dll_remove(t_dlist **list, t_dlist *elem)
