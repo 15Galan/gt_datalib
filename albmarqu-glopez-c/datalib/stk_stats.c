e056b63e5d2844ccecd8642a3aed70a51822ea37
glopez-c <glopez-c@student.42malaga.com>
2024-10-08 17:23:24 +0200

    Added stk_top() to stk_stats.c

--- albmarqu-glopez-c/datalib/stk_stats.c
+++ albmarqu-glopez-c/datalib/stk_stats.c
@@ -6,7 +6,7 @@
 /*   By: glopez-c <glopez-c@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2024/10/08 17:22:57 by glopez-c         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:23:18 by glopez-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,9 @@ int	stk_size(t_stack *stack)
 
 t_stack	*stk_top(t_stack *stack)
 {
-	// TODO
+	if (stk_empty(stack))
+		return (NULL);
+	return (stack);
 }
 
 void	*stk_peek(t_stack *stack)
