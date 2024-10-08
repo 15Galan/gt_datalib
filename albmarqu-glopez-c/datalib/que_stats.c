b3393b0b87ad2ea8f5bb614874f1e5ccd4349e14
albmarqu <albmarqu@student.42malaga.com>
2024-10-08 17:24:57 +0200

    Function: search in the queue

--- albmarqu-glopez-c/datalib/que_stats.c
+++ albmarqu-glopez-c/datalib/que_stats.c
@@ -6,7 +6,7 @@
 /*   By: albmarqu <albmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2024/10/08 17:24:20 by albmarqu         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:24:44 by albmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,11 @@ t_queue	*que_last(t_queue *queue)
 
 t_queue	*que_search(t_queue *queue, void *data)
 {
-
+	while (queue)
+	{
+		if (queue->data == data)
+			return (queue);
+		queue = queue->next;
+	}
+	return (NULL);
 }
