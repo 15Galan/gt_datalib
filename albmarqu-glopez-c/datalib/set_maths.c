bb67ee8f288f70fc3c45e747a5e109a647d5667b
glopez-c <glopez-c@student.42malaga.com>
2024-10-08 17:44:11 +0200

    Added set_inter() to set_maths.c

--- albmarqu-glopez-c/datalib/set_maths.c
+++ albmarqu-glopez-c/datalib/set_maths.c
@@ -6,7 +6,7 @@
 /*   By: glopez-c <glopez-c@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:48:46 by antgalan          #+#    #+#             */
-/*   Updated: 2024/10/08 17:43:33 by glopez-c         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:44:04 by glopez-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,18 @@ t_set	*set_union(t_set *set1, t_set *set2)
 
 t_set	*set_inter(t_set *set1, t_set *set2)
 {
-	// TODO
+	t_set	*aux;
+	t_set	*new;
+
+	new = NULL;
+	aux = set1;
+	while (aux)
+	{
+		if (set_contains(set2, aux->data))
+			set_add(&new, aux->data);
+		aux = aux->next;
+	}
+	return (new);
 }
 
 t_set	*set_diff(t_set *set1, t_set *set2)
