4b8204edbc4e60181ac9e931924889f23dfdd46d
albmarqu <albmarqu@student.42malaga.com>
2024-10-08 17:41:54 +0200

    Function: add after in dll

--- albmarqu-glopez-c/datalib/dll_creators.c
+++ albmarqu-glopez-c/datalib/dll_creators.c
@@ -6,7 +6,7 @@
 /*   By: albmarqu <albmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2024/10/08 17:41:06 by albmarqu         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:41:33 by albmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,13 @@ void	dll_add_first(t_dlist **list, t_dlist *elem)
 
 void	dll_add_after(t_dlist *elem, t_dlist *new)
 {
-	// TODO
+	if (dll_empty(elem) || dll_empty(new))
+		return ;
+	new->next = elem->next;
+	new->prev = elem;
+	if (elem->next)
+		elem->next->prev = new;
+	elem->next = new;
 }
 
 void	dll_add_last(t_dlist **list, t_dlist *elem)
