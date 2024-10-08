6cfe06980dee83848cf7b1dc62c511a5ddf89eb0
albmarqu <albmarqu@student.42malaga.com>
2024-10-08 17:08:10 +0200

    Function: new queue

--- albmarqu-glopez-c/datalib/que_basics.c
+++ albmarqu-glopez-c/datalib/que_basics.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   que_basics.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbruzzi <mbruzzi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: albmarqu <albmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/06/27 13:54:57 by mbruzzi          ###   ########.fr       */
+/*   Updated: 2024/10/08 17:07:14 by albmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,35 +26,15 @@ t_queue	*que_new(void *data)
 
 void	que_enqueue(t_queue **queue, void *data)
 {
-	t_queue	*new;
 
-	new = que_new(data);
-	if (!new)
-		return ;
-	if (que_empty(*queue))
-	{
-		*queue = new;
-		return ;
-	}
-	que_last(*queue)->next = new;
 }
 
 void	*que_dequeue(t_queue **queue)
 {
-	t_queue	*aux;
-	void	*data;
 
-	if (que_empty(*queue))
-		return (NULL);
-	aux = *queue;
-	*queue = (*queue)->next;
-	data = aux->data;
-	free(aux);
-	return (data);
 }
 
 void	que_clear(t_queue **queue)
 {
-	while (!que_empty(*queue))
-		que_dequeue(queue);
+
 }
