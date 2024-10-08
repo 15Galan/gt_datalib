9f4173803f536f29b7ec035cda67306e3bbdcd62
albmarqu <albmarqu@student.42malaga.com>
2024-10-08 17:23:06 +0200

    Function: empty queue

--- albmarqu-glopez-c/datalib/que_stats.c
+++ albmarqu-glopez-c/datalib/que_stats.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   que_stats.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbruzzi <mbruzzi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: albmarqu <albmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/06/27 14:16:28 by mbruzzi          ###   ########.fr       */
+/*   Updated: 2024/10/08 17:22:36 by albmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,36 +19,20 @@ int	que_empty(t_queue *queue)
 
 int	que_size(t_queue *queue)
 {
-	int	i;
 
-	i = 0;
-	while (queue)
-	{
-		queue = queue->next;
-		i++;
-	}
-	return (i);
 }
 
 t_queue	*que_first(t_queue *queue)
 {
-	return (queue);
+
 }
 
 t_queue	*que_last(t_queue *queue)
 {
-	while (queue && queue->next)
-		queue = queue->next;
-	return (queue);
+
 }
 
 t_queue	*que_search(t_queue *queue, void *data)
 {
-	while (queue)
-	{
-		if (queue->data == data)
-			return (queue);
-		queue = queue->next;
-	}
-	return (NULL);
+
 }
